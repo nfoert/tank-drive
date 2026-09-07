0e13b9a3e088b7a281f84af76e55bd63589119d7
nfoert <nofoert@gmail.com>
2026-09-07 19:50:10 -0400

    feat: Set up constants

---
+++ include/constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+namespace DrivetrainConstants {
+    inline std::vector<std::int8_t> LEFT_DRIVETRAIN_PORTS = {0, 0, 0, 0};
+    inline std::vector<std::int8_t> RIGHT_DRIVETRAIN_PORTS = {0, 0, 0, 0};
+};
